47faef39a4b2ecb35e2e6d319cc9f3db72175291
Till-LeFx <t.schure@lefx.de>
2020-07-18 00:39:47 +0200

    .

--- AFrame_AR_Multimarkers-master/js/multiMarkersName.js
+++ AFrame_AR_Multimarkers-master/js/multiMarkersName.js
@@ -29,16 +29,39 @@ AFRAME.registerComponent('markers_start',{
 
 			markerEl.setAttribute('registerevents','');
 			sceneEl.appendChild(markerEl);
-
-			//Adding text to each marker
-			var textEl = document.createElement('a-entity');
 			
-			textEl.setAttribute('id','text');
-			textEl.setAttribute('text',{color: 'red', align: 'center', value:markersNameArray[k], width: '5.5'});
-			textEl.object3D.position.set(0, 0.7, 0);
-			textEl.object3D.rotation.set(-90, 0, 0);
+			if (k==0)
+			{								
+				var videoURL="resources/media/Video_00.mp4";				
+				var myVideo = document.createElement('video');
+				
+				myVideo.setAttribute('id','video0');
+				myVideo.setAttribute('video0',{src: videoURL, autoplay: false, loop crossorigin: 'anonymous'});
+				assetsEl.appendChild(video);
+				sceneEl.appendChild(assetsEl);
+				
+				var videoEl = document.createElement('a-video');
+				console.log('Added Video: ', videoURL);
+				
+				videoEl.setAttribute('id','#video0');				
+				videoEl.setAttribute('video0',{src:'#video0', height: '3', width:'1.69811321', transparent:false});								
+				videoEl.object3D.position.set(0, 0, 0);
+				videoEl.object3D.rotation.set(0, 0, 0);
+				
+				markerEl.appendChild(videoEl);
+			}
+			else
+			{
+				//Adding text to each marker
+				var textEl = document.createElement('a-entity');
+			
+				textEl.setAttribute('id','text');
+				textEl.setAttribute('text',{color: 'red', align: 'center', value:markersNameArray[k], width: '5.5'});
+				textEl.object3D.position.set(0, 0.7, 0);
+				textEl.object3D.rotation.set(-90, 0, 0);
 
-			markerEl.appendChild(textEl);
+				markerEl.appendChild(textEl);
+			}
 		}
 	}
 });
