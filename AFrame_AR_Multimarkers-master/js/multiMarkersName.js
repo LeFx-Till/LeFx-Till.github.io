21e323c7544823c9f83f2aef05cb8d36fc851c53
Till-LeFx <t.schure@lefx.de>
2020-07-17 22:47:36 +0200

    .

--- AFrame_AR_Multimarkers-master/js/multiMarkersName.js
+++ AFrame_AR_Multimarkers-master/js/multiMarkersName.js
@@ -31,17 +31,16 @@ AFRAME.registerComponent('markers_start',{
 			sceneEl.appendChild(markerEl);
 
 			if (k==0)
-			{
-				console.log("k is "+k);
-				//var markerEl = document.createElement('a-marker');
+			{								
 				var videoURL="resources/media/Video_0-"+k+".mp4";
-				var videoEl = document.createElement('a-video');
+				var videoEl = document.createElement('a-entity');
+				videoEl.setAttribute('id','video');
 				videoEl.setAttribute('src',videoURL);
 				videoEl.setAttribute('height',3);
 				videoEl.setAttribute('width',1.69811321);
 				videoEl.setAttribute('transparent',false);
-				videoEl.object3D.position.set(0, 0, 0);
-				videoEl.object3D.rotation.set(0, 0, 0);
+				videoEl.object3D.position.set(0, 0.7, 0);
+				videoEl.object3D.rotation.set(-90, 0, 0);
 				markerEl.appendChild(videoEl);
 			}
 			else
