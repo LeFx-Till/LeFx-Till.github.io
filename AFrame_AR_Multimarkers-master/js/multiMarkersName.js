23d5476d13a2216c7ae26ec599ae09377f56487a
Till-LeFx <t.schure@lefx.de>
2020-07-17 21:53:57 +0200

    .

--- AFrame_AR_Multimarkers-master/js/multiMarkersName.js
+++ AFrame_AR_Multimarkers-master/js/multiMarkersName.js
@@ -38,7 +38,7 @@ AFRAME.registerComponent('markers_start',{
 				videoEl.setAttribute('height',3);
 				videoEl.setAttribute('src',1.69811321);
 				videoEl.setAttribute('color',"#FFFFFF");
-				videoEl.setAttribute('transparent',False);
+				videoEl.setAttribute('transparent',"false");
 				videoEl.object3D.position.set(0, 0, 0);
 				videoEl.object3D.rotation.set(0, 0, 0);
 				
