5a45bf2b1c821e56fc05da193def399c91db2706
Till-LeFx <t.schure@lefx.de>
2020-07-29 21:21:04 +0200

    .

---
+++ AR-js/src/multiMarkerHandler.js
@@ -0,0 +1,16 @@
+////Detect marker found and lost
+//AFRAME.registerComponent('registerevents', {
+//    init: function () {
+//        const marker = this.el;
+
+//        marker.addEventListener("markerFound", () => {
+//            var markerId = marker.id;
+//            console.log('Marker Found By Super Till_01: ', markerId);
+//        });
+
+//        marker.addEventListener("markerLost", () => {
+//            var markerId = marker.id;
+//            console.log('Marker Lost: ', markerId);
+//        });
+//    },
+//});
